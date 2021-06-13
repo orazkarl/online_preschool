20ab48988bd947f55d324ac06aee2d42f5e580fb
orazkarl <orazkarl>
2021-06-13 15:39:12 +0600

    News done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view())
+    path('', views.HomeView.as_view()),
+
+    path('news/', views.NewsListView.as_view(), name='news_list'),
+    path('news/<int:pk>', views.NewsDetailView.as_view(), name='news_detail'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import News
+
 
 class HomeView(generic.TemplateView):
     template_name = 'mainapp/home.html'
+
+
+class NewsListView(generic.ListView):
+    model = News
+    # paginate_by = 10
+
+class NewsDetailView(generic.DetailView):
+    model = News
