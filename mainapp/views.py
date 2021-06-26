cda8a93d89bea59a48a7620982c80e1fad484b5f
orazkarl <orazkarl>
2021-06-27 01:52:07 +0600

    Student front - profile done

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,13 +9,15 @@ class HomeView(generic.TemplateView):
     template_name = 'mainapp/home.html'
 
     def get(self, request, *args, **kwargs):
-        if Student.objects.filter(user=request.user).exists():
-            return redirect('student_profile')
-        elif Teacher.objects.filter(user=request.user).exists():
-            return redirect('teacher_profile')
-        elif request.user.is_superuser:
-            return redirect('/admin')
-        redirect('news_list')
+        if str(request.user) != 'AnonymousUser':
+            if Student.objects.filter(user=request.user).exists():
+                return redirect('student_profile')
+            elif Teacher.objects.filter(user=request.user).exists():
+                return redirect('teacher_profile')
+            elif request.user.is_superuser:
+                return redirect('/admin')
+            return redirect('news_list')
+        return redirect('account_login')
 
 class NewsListView(generic.ListView):
     model = News
